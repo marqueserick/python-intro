1542d21a91253d5421924b3ecc27abb98e431164
marqueserick <erickmarques43@gmail.com>
2022-05-30 18:49:46 -0300

    primeiro commit

---
+++ metodo_print.py
@@ -0,0 +1,8 @@
+#print parametro sep define o que separa os parametros enviados e end define como termina a funcao print
+print("Itália","ganhou","4","títulos", sep=" - ", end="!\n")
+pais = 'Itália'
+
+#type mostra o tipo da variavel
+print(type(pais))
+
+print(pais,"ganhou","4","títulos", end="!\n")
