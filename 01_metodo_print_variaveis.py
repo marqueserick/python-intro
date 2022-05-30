9b2e1a90ba42b8c95ba05cf9b87db3568fab3118
marqueserick <erickmarques43@gmail.com>
2022-05-30 19:23:18 -0300

    adiciona jogo adivinhacao

--- 01_metodo_print_variaveis.py
+++ 01_metodo_print_variaveis.py
@@ -6,3 +6,11 @@ pais = 'Itália'
 print(type(pais))
 
 print(pais,"ganhou","4","títulos", end="!\n")
+
+#variaveis possum tipagem dinamicas
+pais = 7.9
+print(type(pais))
+
+#nomes de variaveis seguem o padrao Snake_Case
+idade_filho = 10
+nome_social = "Ana"
---
+++ jogo_adivinhacao.py
@@ -0,0 +1,10 @@
+from random import Random as rd
+print("Bem-vindo ao jogo de adivinhação!!!")
+numero_secreto = 55
+chute = int(input("Digite um número: "))
+print("Você digitou",chute)
+
+if (numero_secreto == chute):
+    print("Você acertou!!!")
+else:
+    print("Você errou :(")
