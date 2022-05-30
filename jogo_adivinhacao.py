f0f7aedcb1bb14c89a160189810e3066a862191e
marqueserick <erickmarques43@gmail.com>
2022-05-30 20:08:47 -0300

    condicao elif

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -1,10 +1,18 @@
 
 print("Bem-vindo ao jogo de adivinhação!!!")
 numero_secreto = 55
-chute = int(input("Digite um número: "))
+chute_str = input("Digite um número: ")
+chute = int(chute_str)
+maior = chute>numero_secreto
+menor = chute<numero_secreto
+
 print("Você digitou",chute)
 
 if (numero_secreto == chute):
     print("Você acertou!!!")
 else:
-    print("Você errou :(")
+    print("Você errou!!! :(", end=" ")
+    if(maior):
+        print("Seu chute foi maior que o número secreto")
+    elif(menor):
+        print("Seu chute foi menor que o número secreto")
