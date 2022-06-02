f2608f3981e1734ea6e09f22d2ed107117a5eff1
marqueserick <erickmarques43@gmail.com>
2022-06-02 20:14:51 -0300

    adiciona pontuacao

--- jogo_adivinhacao_laco_for.py
+++ jogo_adivinhacao_laco_for.py
@@ -3,6 +3,8 @@ print("Bem-vindo ao jogo de adivinhação!!!")
 #numero_secreto = int(rd.uniform(0,1)*100)
 #numero_secreto = rd.randint(1,100)
 numero_secreto = rd.randrange(1,101)
+pontos = 1000
+pontos_perdidos = 0
 
 nivel = input("Selecione um nível de dificuldade\n(1) Fácil (2) Médio (3) Difícil\n")
 if nivel == '1':
@@ -13,6 +15,8 @@ elif nivel == '3':
     total_tentativas = 3
 else:
     total_tentativas = 1
+
+constante_precisao = 10 / total_tentativas
 
 for rodada in range(1,(total_tentativas+1)):
     print("Tentativa {} de {}".format(rodada,total_tentativas))
@@ -25,14 +29,18 @@ for rodada in range(1,(total_tentativas+1)):
     print("Você digitou",chute)
 
     if chute < 1 or chute > 100:
+        pontos_perdidos += (abs(chute-numero_secreto) * constante_precisao)
         print("Você deve digitar um número entre 1 e 100", end="\n\n")
         continue
 
     if (acertou):
         print("Você acertou!!!")
+        precisao = (pontos - pontos_perdidos) * 100 / pontos
+        print("Sua precisão foi de {:.2f}%".format(precisao))
         break
 
     else:
+        pontos_perdidos += (abs(chute-numero_secreto) * constante_precisao)
         print("Você errou!!! :(", end=" ")
         if(maior):
             print("Seu chute foi maior que o número secreto", end="\n\n")
@@ -41,3 +49,5 @@ for rodada in range(1,(total_tentativas+1)):
             print("Seu chute foi menor que o número secreto", end="\n\n")
 else:
     print("Fim de Jogo\nO número secreto era {}".format(numero_secreto))
+    precisao = (pontos - pontos_perdidos) * 100 / pontos
+    print("Sua precisão foi de {:.2f}%".format(precisao))
