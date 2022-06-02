9b338b86d0d3c9e2477a5ad887aeacef94e1d0d5
marqueserick <erickmarques43@gmail.com>
2022-06-01 21:56:57 -0300

    adiciona formata string

--- jogo_adivinhacao_laco_for.py
+++ jogo_adivinhacao_laco_for.py
@@ -1,10 +1,10 @@
 print("Bem-vindo ao jogo de adivinhação!!!")
 numero_secreto = 55
-total_tentativas = 3
+total_tentativas = 8
 89
 for rodada in range(1,(total_tentativas+1)):
     print("Tentativa {} de {}".format(rodada,total_tentativas))
-    chute_str = input("Digite um número: ")
+    chute_str = input("Digite um número de 1 a 100: ")
     chute = int(chute_str)
     acertou = numero_secreto == chute
     maior = chute>numero_secreto
@@ -12,13 +12,19 @@ for rodada in range(1,(total_tentativas+1)):
 
     print("Você digitou",chute)
 
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100")
+        continue
+
     if (acertou):
         print("Você acertou!!!")
         break;
+
     else:
         print("Você errou!!! :(", end=" ")
         if(maior):
             print("Seu chute foi maior que o número secreto")
+
         elif(menor):
             print("Seu chute foi menor que o número secreto")
 else:
