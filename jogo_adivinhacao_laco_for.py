21c5d90b2cb666751a194a9c1d8f30f238a8594e
marqueserick <erickmarques43@gmail.com>
2022-06-02 19:32:15 -0300

    adiciona niveis

--- jogo_adivinhacao_laco_for.py
+++ jogo_adivinhacao_laco_for.py
@@ -1,9 +1,19 @@
 import random as rd
 print("Bem-vindo ao jogo de adivinhação!!!")
-numero_secreto = int(rd.uniform(0,1)*100)
-numero_secreto_randint = rd.randint(1,100)
-total_tentativas = 3
-89
+#numero_secreto = int(rd.uniform(0,1)*100)
+#numero_secreto = rd.randint(1,100)
+numero_secreto = rd.randrange(1,101)
+
+nivel = input("Selecione um nível de dificuldade\n(1) Fácil (2) Médio (3) Difícil\n")
+if nivel == '1':
+    total_tentativas = 10
+elif nivel == '2':
+    total_tentativas = 5
+elif nivel == '3':
+    total_tentativas = 3
+else:
+    total_tentativas = 1
+
 for rodada in range(1,(total_tentativas+1)):
     print("Tentativa {} de {}".format(rodada,total_tentativas))
     chute_str = input("Digite um número de 1 a 100: ")
@@ -15,7 +25,7 @@ for rodada in range(1,(total_tentativas+1)):
     print("Você digitou",chute)
 
     if chute < 1 or chute > 100:
-        print("Você deve digitar um número entre 1 e 100")
+        print("Você deve digitar um número entre 1 e 100", end="\n\n")
         continue
 
     if (acertou):
@@ -25,9 +35,9 @@ for rodada in range(1,(total_tentativas+1)):
     else:
         print("Você errou!!! :(", end=" ")
         if(maior):
-            print("Seu chute foi maior que o número secreto")
+            print("Seu chute foi maior que o número secreto", end="\n\n")
 
         elif(menor):
-            print("Seu chute foi menor que o número secreto")
+            print("Seu chute foi menor que o número secreto", end="\n\n")
 else:
     print("Fim de Jogo\nO número secreto era {}".format(numero_secreto))
