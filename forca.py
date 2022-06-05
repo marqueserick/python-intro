ee466e1a0713ffa93d3bc657edd515cbe0f964e2
marqueserick <erickmarques43@gmail.com>
2022-06-05 13:53:02 -0300

    adiciona jogo forca

---
+++ forca.py
@@ -0,0 +1,12 @@
+def jogar():
+    print("Bem-vindo ao jogo de Forca")
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
+
+
+if __name__ == "__main__":
+    jogar()
--- menu_jogos.py
+++ menu_jogos.py
@@ -1,10 +1,14 @@
+from turtle import end_fill
 import adivinhacao
+import forca
 
 print("""#######################
 Bem-vindo ao menu de jogos
 #######################""")
-op = input("Pressione 1 para jogar o Adivinhação")
+op = input("Pressione 1 para jogar Adivinhação\nPressione 2 para jogar Forca\n")
 if op =='1':
     adivinhacao.jogar()
+elif op =='2':
+    forca.jogar()
 else:
     print("Fechando o menu de jogos...")
