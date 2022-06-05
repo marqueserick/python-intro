0659ba8fca4fbc16e5e7fd7407b68531908f0800
marqueserick <erickmarques43@gmail.com>
2022-06-05 18:49:08 -0300

    adiciona jogo Forca

--- forca.py
+++ forca.py
@@ -1,12 +1,57 @@
 def jogar():
     print("Bem-vindo ao jogo de Forca")
-    palavra_secreta = "banana"
+    palavra_secreta = "caderno".upper()
+    letras_acertadas = []
+    acertos = 0
+    erros = 0
     enforcou = False
     acertou = False
+    
+
+    for i in range(len(palavra_secreta)):
+        letras_acertadas.append("_")
+    print("Palavra secreta:",palavra_mascarada(letras_acertadas), end="\n\n")
+    nivel = input("Selecione um nível de dificuldade\n(1) Fácil (2) Médio (3) Difícil\n")
+    if nivel == '1':
+        total_tentativas = 7
+    elif nivel == '2':
+        total_tentativas = 5
+    elif nivel == '3':
+        total_tentativas = 3
+    else:
+        total_tentativas = 1
 
     while(not enforcou and not acertou):
-        print("jogando...")
+        print("Resta(m) {} tentativa(s)".format((total_tentativas-erros)))
+        chute = input("Escolha uma letra... ").upper()
+        if len(chute) > 1:
+            chute = chute.strip()[0]
+        
+        if chute in palavra_secreta:
+            for i in range(len(palavra_secreta)):
+                if palavra_secreta[i] == chute:
+                    acertos+=1
+                    letras_acertadas[i] = chute
+                    if acertos == len(palavra_secreta):
+                        acertou = True
+                        print("Parabéns, você descobriu a palavra secreta")
+        else:
+            erros+=1
+            if erros == total_tentativas:
+                print("Você foi enforcado")
+                enforcou = True
 
+        if enforcou:
+            print("A palavra secreta era {}".format(palavra_secreta))
+        else:
+            print(palavra_mascarada(letras_acertadas), end="\n\n")
+    else:
+        print("Fim de jogo!")
 
+def palavra_mascarada(palavra):
+    mascara =""
+    for p in palavra:
+        mascara = mascara + p + " "
+    return mascara
 if __name__ == "__main__":
     jogar()
